ee5b91b4665a8305aa55ba4e6bb7dab0edcb4284
陈锦清 <hitearth@163.com>
2015-03-20 15:50:47 +0800

    add server2
    计时器每次执行时采用多线程处理作业

---
+++ Server2/Service1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+using Hitearth.Tool;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Hitearth
+{
+    public partial class Service1 : ServiceBase
+    {
+        private TimerManager<string> timerManager;
+        private List<string> users = new List<string>();
+        public Service1()
+        {
+            InitializeComponent();
+            timerManager = new TimerManager<string>(timer1, dosomething, users);
+            timerManager.Log += new Action<string>(C.WriteLineLog);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Ini();
+        }
+        public void Ini()
+        {
+            try
+            {
+
+                utils.WriteLog("-------------------------------OnStart-------------------------------");
+
+                #region Log
+                if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\Log"))
+                {
+                    Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\Log");
+                }
+                #endregion
+
+                #region Remoting
+                RemotingConfiguration.Configure(AppDomain.CurrentDomain.BaseDirectory + "\\channels.config", true);
+                foreach (IChannelReceiver item in ChannelServices.RegisteredChannels)
+                {
+                    utils.WriteLog(item.GetType().ToString() + "\t" + string.Join("\t", item.GetUrlsForUri("")));
+                }
+                #endregion
+
+                users.AddRange(new[] { "张一", "张二", "张三", "张四" });
+
+                timerManager.IniTimer(Config.timer1Interval, true);
+
+                utils.WriteLog("-------------------------------OnStart-------------------------------");
+            }
+            catch (Exception ex)
+            {
+                C.WriteLog(ex.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                foreach (IChannelReceiver item in ChannelServices.RegisteredChannels)
+                {
+                    ChannelServices.UnregisterChannel(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                utils.WriteLog(ex.ToString());
+            }
+        }
+
+        public void dosomething(string obj)
+        {
+            // do something
+            C.WriteLineLog(obj + " do something");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Server2/Tool/TimerManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Timer = System.Timers.Timer;
+
+
+namespace Hitearth.Tool
+{
+    public class TimerManager<T>
+    {
+        private Timer timer1; private DealPost dealPost; private IEnumerable<T> Ports;
+        private Action<string> onLog;
+        private void myLog(string content)
+        {
+            if (onLog != null) onLog(content);
+        }
+        public event Action<string> Log
+        {
+            add
+            {
+                onLog += value;
+            }
+            remove
+            {
+                onLog -= value;
+            }
+
+        }
+        public TimerManager(Timer timer1, DealPost dealPost, IEnumerable<T> Ports)
+        {
+            if (timer1 == null) throw new ArgumentNullException("timer1");
+            if (dealPost == null) throw new ArgumentNullException("dealPost");
+            if (Ports == null) throw new ArgumentNullException("Ports");
+
+            this.timer1 = timer1;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.Elapsed);
+            this.dealPost = dealPost; this.Ports = Ports;
+        }
+        public void IniTimer(double timerInterval, bool enabled)
+        {
+            timer1.Interval = timerInterval;
+            timer1.Enabled = enabled;
+        }
+        private static object timer1obj = new object();
+        public void Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer1.Enabled = false;
+            lock (timer1obj)
+            {
+                var beginTime = DateTime.Now;
+                myLog(Thread.CurrentThread.ManagedThreadId.ToString() + "定时器开始执行");
+                ManualResetEvent[] events = new ManualResetEvent[Ports.Count()]; int i = 0;
+                foreach (var port in Ports)
+                {
+                    events[i] = new ManualResetEvent(false);
+                    WaitCallback callback = new WaitCallback(dealWork);
+                    ThreadPool.QueueUserWorkItem(callback, new object[] { events[i], port });
+                    i++;
+                }
+                ManualResetEvent.WaitAll(events);
+                var dd = DateTime.Now - beginTime;
+                myLog(Thread.CurrentThread.ManagedThreadId.ToString() + "定时器结束执行\t历时" + dd.TotalMilliseconds + "毫秒");
+            }
+            timer1.Enabled = true;
+        }
+        public delegate void DealPost(T port);
+        public void dealWork(object state)
+        {
+            object[] obj = state as object[];
+            ManualResetEvent resetEvent = ((ManualResetEvent)obj[0]);
+            T port = (T)(obj[1]);
+            try
+            {
+                dealPost(port);
+            }
+            finally
+            {
+                resetEvent.Set();
+            }
+        }
+    }
+}
